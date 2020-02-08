cb1964a91f56ba1667d55ac9330ff4f0cbe02c32
Jo Seong-cheol <josworks27@gmail.com>
2020-02-08 13:32:49 +0900

    Create Image Upload into AWS S3

--- src/routes/houses.ts
+++ src/routes/houses.ts
@@ -2,22 +2,44 @@ import { Router } from 'express';
 import * as housesController from '../controllers/houses';
 import * as reviewController from '../controllers/review';
 import * as multer from 'multer';
+import * as multerS3 from 'multer-s3';
 import * as path from 'path';
+import * as AWS from 'aws-sdk';
 
 export const housesRouter = Router();
 
+// Image Upload into AWS S3
+AWS.config.loadFromPath(
+  '/Users/joseongcheol/Desktop/code_states_im16/immersive16/07_final_project/BillyZip-server/src/config/awsconfig.json',
+);
+
+const s3 = new AWS.S3();
+
 const upload = multer({
-  storage: multer.diskStorage({
-    // set a localstorage destination
-    destination: (req, file, cb) => {
-      cb(null, './src/uploads/');
+  storage: multerS3({
+    s3: s3,
+    bucket: 'billyzip',
+    key: function(req, file, cb) {
+      const extension = path.extname(file.originalname);
+      cb(null, Date.now().toString() + extension);
     },
-    // convert a file name
-    filename: (req, file, cb) => {
-      cb(null, new Date().valueOf() + path.extname(file.originalname));
-    },
+    acl: 'public-read-write',
   }),
 });
+
+// Image Upload into /uploads
+// const upload = multer({
+//   storage: multer.diskStorage({
+//     // set a localstorage destination
+//     destination: (req, file, cb) => {
+//       cb(null, './src/uploads/');
+//     },
+//     // convert a file name
+//     filename: (req, file, cb) => {
+//       cb(null, new Date().valueOf() + path.extname(file.originalname));
+//     },
+//   }),
+// });
 
 // housesController
 housesRouter.post('/', upload.array('images', 3), housesController.PostHouse);
