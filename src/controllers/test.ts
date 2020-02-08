f5294412d675c5f7165b03ce058137d2cd2e6e21
Jo Seong-cheol <josworks27@gmail.com>
2020-02-09 02:45:02 +0900

    fix typo

--- src/controllers/test.ts
+++ src/controllers/test.ts
@@ -10,16 +10,32 @@ const client = twilio(accountSid, authToken);
 
 // GET
 // /test
-export const GetTest = async (req: Request, res: Response) => {
-  client.messages
-    .create({
-      body: 'BillyZip 회원가입 인증번호 전송!!',
-      to: '+821027441488', // Text this number
-      from: '+17014017638', // From a valid Twilio number
-    })
-    .then((message) => {
-      console.log(message.sid);
-      // SM03c2de5a81354deb88c723e26d05eb78
-      res.end();
-    });
+export const getTest = (req: Request, res: Response) => {
+  // 유저가 인증번호를 요청할 때의 로직
+  // GET으로 요청을 받으면 유저의 핸드폰 번호로 인증번호를 보내준다.
+  const messaageResult = client.messages.create({
+    body: 'BillyZip 회원가입 인증번호 전송!!',
+    to: '+821027441488', // Text this number
+    from: '+17014017638', // From a valid Twilio number
+  });
+
+  messaageResult.then((message) => {
+    console.log(message.sid);
+  });
+
+  res.end();
+};
+
+// POST
+// /test
+export const postTest = (req: Request, res: Response) => {
+  // 인증번호를 확인한 유저가 POST 요청으로 인증번호를 보낸다.
+  // req.body로 인증번호를 확인하고 대조작업을 한다 ?
+  
+  // if (인증번호가 맞을 경우) {
+  // } else {
+  // 인증번호가 맞지 않을 경우
+  // }
+
+  res.end();
 };
--- src/routes/test.ts
+++ src/routes/test.ts
@@ -2,4 +2,4 @@ import { Router } from 'express';
 import * as testController from '../controllers/test';
 export const testRouter = Router();
 
-testRouter.get('/', testController.GetTest);
+testRouter.get('/', testController.getTest);
