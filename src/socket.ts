f97a3faeeef0a2d5439972720a9ecc3907751617
Jo Seong-cheol <josworks27@gmail.com>
2020-02-19 01:00:30 +0900

    Modify GET /favs: Add house.images property

--- src/socket.ts
+++ src/socket.ts
@@ -4,30 +4,26 @@ import app from './index';
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
 
-const room = ['room1', 'room2'];
-let a = 0;
 io.on('connection', (socket: any) => {
-  // socket.on('chat', (data: any) => {
-  //   console.log('클라이언트에서 온 메시지: ', data.msg);
-  //   const msg = {
-  //     msg: data.msg,
-  //   };
-  //   io.emit('chat', msg);
-  // });
-
   // 1:1 룸에 조인하기
-  socket.on('joinRoom', (num: number, name: string) => {
-    socket.join(num, () => {
-      console.log(name + ' join a ' + num);
-
-      io.to(num).emit('joinRoom', num, name);
+  socket.on('joinRoom', (userId: string) => {
+    socket.join(userId, () => {
+      console.log(`서버 메세지: ${userId}의 룸입니다.`);
     });
   });
 
   // 1:1 룸으로 메세지 보내기
-  socket.on('chat', (num: number, name: string, msg: string) => {
-    io.to(num).emit('chat', name, msg);
+  socket.on('chat', (userId: string, msg: string) => {
+    io.to(userId).emit('chat', msg);
   });
 });
 
 export default server;
+
+// socket.on('chat', (data: any) => {
+//   console.log('클라이언트에서 온 메시지: ', data.msg);
+//   const msg = {
+//     msg: data.msg,
+//   };
+//   io.emit('chat', msg);
+// });
