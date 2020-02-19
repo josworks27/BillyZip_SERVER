253dccfcc3360b462640eee6f1b6efea9db74ba0
Jo Seong-cheol <josworks27@gmail.com>
2020-02-20 03:30:38 +0900

    Modify forum

--- src/controllers/forum.ts
+++ src/controllers/forum.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express';
-import authHelper from '../util/authHelper';
 import { Forum } from '../entities/Forum';
 import { getRepository, getConnection } from 'typeorm';
 import { JoinForum } from '../entities/JoinForum';
+import authHelper from '../util/authHelper';
+import { User } from '../entities/User';
 
 // * POST
 // * /forum
@@ -12,6 +13,13 @@ export const PostForum = async (req: Request, res: Response) => {
   if (authResult.decode) {
     const { messages, myId, hostId } = req.body;
     console.log('? ', messages, myId, hostId);
+
+    // 호스트 이름 찾기
+    const hostInfo: any = await getRepository(User).findOne({
+        where: {
+            id: hostId
+        }
+    });
 
     // 포럼 메세지 존재하는지 확인후 메세지 저장하기
     // ! 없으면 디비에 insert하고
@@ -28,6 +36,7 @@ export const PostForum = async (req: Request, res: Response) => {
       const newForum = new Forum();
       newForum.hostId = hostId;
       newForum.forumLog = JSON.stringify(messages);
+      newForum.isActive = true;
       await newForum.save();
     } else {
       // 포럼이 있을 때 => 갱신
@@ -57,9 +66,11 @@ export const PostForum = async (req: Request, res: Response) => {
       const newJoinForum = new JoinForum();
       newJoinForum.userId = myId;
       newJoinForum.hostId = hostId;
+      newJoinForum.hostName = hostInfo.name;
+      newJoinForum.isActive = true;
       await newJoinForum.save();
     } else {
-        console.log('조인포럼 있을 때 실행');
+      console.log('조인포럼 있을 때 실행');
     }
 
     res.sendStatus(200);
@@ -68,9 +79,43 @@ export const PostForum = async (req: Request, res: Response) => {
   }
 };
 
-// ! 없으면 디비에 insert하고
-// ! 있으면 디비에 update하기
+// * POST
+// * /forum/room
+export const PostForumRoom = async (req: Request, res: Response) => {
+  const authResult = authHelper(req.headers.authorization);
 
-// * 새로운 엔티티 필요:
-// * 1) id / hostId / userId
-// * 2) id / hostId  forumLog
+  if (authResult.decode) {
+    // 호스트아이디 받아서 forum에서 로그 가져와서 응답하기
+    const { hostId } = req.body;
+
+    const forumLog: any = await getRepository(Forum).findOne({
+      where: {
+        hostId: hostId,
+      },
+    });
+
+    res.status(200).json(forumLog);
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+// * POST
+// * /forum/list
+export const PostForumList = async (req: Request, res: Response) => {
+  const authResult = authHelper(req.headers.authorization);
+
+  if (authResult.decode) {
+    // 유저 아이디 받아서 그 유저가 참가한 모든 포럼을 가져온다.
+    const { myId } = req.body;
+
+    const forumList: any = await getRepository(JoinForum).find({
+      where: {
+        userId: myId,
+      },
+    });
+    res.status(200).json(forumList);
+  } else {
+    res.sendStatus(401);
+  }
+};
--- src/routes/forum.ts
+++ src/routes/forum.ts
@@ -3,3 +3,5 @@ import * as forumController from '../controllers/forum';
 export const forumRouter = Router();
 
 forumRouter.post('/', forumController.PostForum);
+forumRouter.post('/room', forumController.PostForumRoom);
+forumRouter.post('/list', forumController.PostForumList);
