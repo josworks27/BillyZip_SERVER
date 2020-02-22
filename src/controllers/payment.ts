b685bd6e1c45644952a0f6b6c26ca7480d9bdadc
Jo Seong-cheol <josworks27@gmail.com>
2020-02-22 16:10:02 +0900

    Add try ...catch

--- src/controllers/payment.ts
+++ src/controllers/payment.ts
@@ -10,43 +10,49 @@ import twilioHelper from '../util/twilioHelper';
 export const PostPayment = async (req: Request, res: Response) => {
   // 클라이언트로 부터 결제정보 받아서 payment 디비에 저장
   // 필요한 정보: userId(with TOKEN) / subscribePlan / paymentDate / paymentOption
-
   const authResult = authHelper(req.headers.authorization);
 
   if (authResult.decode) {
     const { subscribePlan, paymentDate, paymentOption } = req.body;
     const { userId } = authResult.decode;
-    // ! 디비: user에 currentPlan 업데이트 하기 / payment에 위 정보 생성하기
 
-    if (paymentOption === undefined) {
-      res.sendStatus(400);
+    try {
+      // ! 디비: user에 currentPlan 업데이트 하기 / payment에 위 정보 생성하기
+      // currentPlan 업데이트 하기
+      await getConnection()
+        .createQueryBuilder()
+        .update(User)
+        .set({ currentPlan: subscribePlan })
+        .where('id = :id', { id: userId })
+        .execute();
+
+      // payment에 위 정보 생성하기
+      const currentUser = await User.findOne({ id: userId });
+
+      if (!currentUser) {
+        res.status(404).json({ error: 'currentUser가 존재하지 않습니다.' });
+        return;
+      }
+
+      const newPayment = new Payment();
+      newPayment.user = currentUser;
+      newPayment.subscribePlan = subscribePlan;
+      newPayment.paymentDate = paymentDate;
+      newPayment.paymentOption = paymentOption;
+      newPayment.isActive = true;
+      await newPayment.save();
+
+      // ! Twilio SMS로 유저 전화번호에 결제정보 보내주기
+      await twilioHelper.paymentMsg(
+        currentUser.name,
+        `+82${currentUser.mobile.slice(1)}`,
+      );
+
+      res.sendStatus(200);
+    } catch (err) {
+      console.error('error is ', err);
+      res.status(500).json({ error: err });
     }
-
-    // currentPlan 업데이트 하기
-    await getConnection()
-      .createQueryBuilder()
-      .update(User)
-      .set({ currentPlan: subscribePlan })
-      .where('id = :id', { id: userId })
-      .execute();
-
-    // payment에 위 정보 생성하기
-    const currentUser: any = await User.findOne({ id: userId });
-    const newPayment: any = new Payment();
-    newPayment.user = currentUser;
-    newPayment.subscribePlan = subscribePlan;
-    newPayment.paymentDate = paymentDate;
-    newPayment.paymentOption = paymentOption;
-    newPayment.isActive = true;
-    await newPayment.save();
-
-    // ! Twilio SMS로 유저 전화번호에 결제정보 보내주기
-    await twilioHelper.paymentMsg(
-      currentUser.name,
-      `+82${currentUser.mobile.slice(1)}`,
-    );
-
-    res.sendStatus(200);
   } else {
     res.sendStatus(401);
   }
@@ -56,20 +62,29 @@ export const PostPayment = async (req: Request, res: Response) => {
 // * /payment
 export const GetPayment = async (req: Request, res: Response) => {
   // 토큰으로 userId 확인해서 payment에서 해당 유저의 결제기록 응답하기
-
   const authResult = authHelper(req.headers.authorization);
 
   if (authResult.decode) {
     const { userId } = authResult.decode;
 
-    const payments: any = await getRepository(Payment)
-      .createQueryBuilder('payment')
-      .leftJoinAndSelect('payment.user', 'user')
-      .where('payment.userId = :userId', { userId: userId })
-      .orderBy('payment.created_at', 'DESC')
-      .getMany();
+    try {
+      const payments = await getRepository(Payment)
+        .createQueryBuilder('payment')
+        .leftJoinAndSelect('payment.user', 'user')
+        .where('payment.userId = :userId', { userId: userId })
+        .orderBy('payment.created_at', 'DESC')
+        .getMany();
 
-    res.status(200).json(payments);
+      if (payments.length === 0) {
+        res.status(404).json({ error: 'payments가 존재하지 않습니다.' });
+        return;
+      }
+
+      res.status(200).json(payments);
+    } catch (err) {
+      console.error('error is ', err);
+      res.status(500).json({ error: err });
+    }
   } else {
     res.sendStatus(401);
   }
--- src/util/twilioHelper.ts
+++ src/util/twilioHelper.ts
@@ -8,34 +8,42 @@ const client = twilio(accountSid, authToken);
 const tempAuthObj: { [index: string]: number } = {};
 
 const twilioHelper = {
-  auth: (userPhoneNum: any) => {
+  auth: async (userPhoneNum: any) => {
     const authNumber = Math.floor(Math.random() * 10000);
 
-    tempAuthObj[userPhoneNum] = authNumber;
-    userPhoneNum = `+82${userPhoneNum.slice(1)}`;
+    try {
+      if (!userPhoneNum) throw new Error('잘못된 휴대폰 번호입니다.');
+      tempAuthObj[userPhoneNum] = authNumber;
+      userPhoneNum = `+82${userPhoneNum.slice(1)}`;
 
-    const messaageResult = client.messages.create({
-      body: `BillyZip 회원가입 인증번호 ${authNumber}`,
-      to: userPhoneNum,
-      from: process.env.TWILIO_FROM,
-    });
-
-    messaageResult.then((message) => {
-      console.log(message.sid);
-    });
+      await client.messages.create({
+        body: `BillyZip 회원가입 인증번호 ${authNumber}`,
+        to: userPhoneNum,
+        from: process.env.TWILIO_FROM,
+      });
+    } catch (err) {
+      return err;
+    }
 
     return tempAuthObj;
   },
 
   paymentMsg: async (userName: any, userPhoneNum: any) => {
-    const messaageResult = client.messages.create({
-      body: `${userName}님 요청하신 결제가 정상적으로 완료되었습니다. From BillyZip Team`,
-      to: userPhoneNum,
-      from: process.env.TWILIO_FROM,
-    });
+    try {
+      if (!userName) throw new Error('잘못된 유저 이름입니다.');
+      if (!userPhoneNum) throw new Error('잘못된 휴대폰 번호 입니다.');
 
-    const message = await messaageResult;
-    return message.sid;
+      const messaageResult = await client.messages.create({
+        body: `${userName}님 요청하신 결제가 정상적으로 완료되었습니다. From BillyZip Team`,
+        to: userPhoneNum,
+        from: process.env.TWILIO_FROM,
+      });
+
+      const message = messaageResult;
+      return message.sid;
+    } catch (err) {
+      return err;
+    }
   },
 };
 
