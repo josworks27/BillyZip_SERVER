353c3d8b281538020da08e987d412835ca6149c5
Jo Seong-cheol <josworks27@gmail.com>
2020-03-05 17:59:41 +0900

    Fix cors's origin IP => env

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ app.use(
 app.use(morgan('dev'));
 app.use(
   cors({
-    origin: ['http://172.30.50.202'],
+    origin: [`${process.env.TEST_IP}`],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   }),
